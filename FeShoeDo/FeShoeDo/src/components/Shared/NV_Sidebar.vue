0e9b6bb03f733e9d32535222c2dee42c36990332
NguyenDuyNguyenMinh <beprobro123@gmail.com>
2026-02-24 11:20:22 +0700

    fix code gd

--- FeShoeDo/FeShoeDo/src/components/Shared/NV_Sidebar.vue
+++ FeShoeDo/FeShoeDo/src/components/Shared/NV_Sidebar.vue
@@ -1,11 +1,7 @@
 <template>
   <aside class="sidebar" id="sidebar">
     <div class="sidebar-header">
-<<<<<<< HEAD
-      <h3>BlackWhite</h3>
-=======
       <h3>ShoeDo Shop</h3>
->>>>>>> MINH
       <small>Quản lý cửa hàng của bạn</small>
       
       <!-- User info -->
